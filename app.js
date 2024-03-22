e933ad042dc1b560afbc58eed4711fabcfb0b8bc
adeelmaheen <157056396+adeelmaheen@users.noreply.github.com>
2024-03-22 06:00:00 -0700

    commit

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let car = "toyota";
+let bike = "honda";
+let cycle = "5star";
+let name = " ALI";
+console.log(`${name} like to drive ${car} car and ride to ${bike} bicycle  `);
---
+++ app.ts
@@ -0,0 +1,6 @@
+let car:string="toyota";
+let bike:string="honda";
+let cycle:string= "5star";
+let name:string=" ALI";
+
+console.log(`${name} like to drive ${car} car and ride to ${bike} bicycle  `);
